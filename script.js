cb254d99deaefe49f3a4a60e38e5b4ecd598ebd7
quirinof <matheusquinoff@gmail.com>
2024-01-02 16:55:57 -0300

    update toggleMode

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 function toggleMode() {
   const html = document.documentElement
   const img = document.querySelector("#profile img")
-  
+
   if(html.classList.contains('light')) {
     html.classList.remove('light')
     img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute(
+      'alt', 
+      'Foto de Matheus Quirino com fundo branco. Bigode, sem sorrir, camisa branca.'
+    )
   } else {
     html.classList.add('light')
     img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute(
+      'alt', 
+      'Foto de Matheus Quirino com fundo preto. Óculos escuro, barba, camisa verde.'
+    )
   }
   // html.classList.toggle('light')
 }
