6c66e6548103f86435aadd63bb0aab44ceefe7ff
quirinof <matheusquinoff@gmail.com>
2024-01-02 11:39:00 -0300

    toggle image

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function toggleMode() {
   const html = document.documentElement
-
+  const img = document.querySelector("#profile img")
+  
   if(html.classList.contains('light')) {
     html.classList.remove('light')
+    img.setAttribute('src', './assets/avatar.png')
   } else {
     html.classList.add('light')
+    img.setAttribute('src', './assets/avatar-light.png')
   }
-
   // html.classList.toggle('light')
 }
